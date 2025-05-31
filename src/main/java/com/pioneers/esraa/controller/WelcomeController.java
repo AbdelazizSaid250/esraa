89c27d048338a46bd11321a927dbf194fc8a5b8f
abdelaziz <abdelazizsaid250@gmail.com>
2025-05-31 21:54:49 +0300

    Create an API to welcome any student in the diploma
                   Created a WelcomeController.java class that contains one API to welcome any student in Advanced Java and Spring Boot with Docker diploma.

---
+++ src/main/java/com/pioneers/esraa/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.pioneers.esraa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("welcome")
+public class WelcomeController {
+    
+    @GetMapping("student/{name}")
+    public String welcomeStudentApi(@PathVariable String name) {
+        return "Welcome " + name + " to our Advanced Java and Spring Boot with Docker diploma!!";
+    }
+}
