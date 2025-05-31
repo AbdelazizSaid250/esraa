38359d4277fd55bb96d52bd4a057892a63918c95
abdelaziz <abdelazizsaid250@gmail.com>
2025-05-31 21:12:22 +0300

    Initial commit

---
+++ src/main/java/com/pioneers/esraa/EsraaApplication.java
@@ -0,0 +1,13 @@
+package com.pioneers.esraa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsraaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsraaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pioneers/esraa/EsraaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pioneers.esraa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsraaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
